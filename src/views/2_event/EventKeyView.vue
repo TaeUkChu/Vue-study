a032ce5f225ba320d8961eba2ed78637edad8217
TaeUkChu <hippo2012@navver.com>
2023-03-16 15:19:31 +0900

    finish

---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input
+      type="search"
+      name=""
+      id=""
+      @keyup.enter="doSearch"
+      v-model="searchText"
+    />
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch">제출</button>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
